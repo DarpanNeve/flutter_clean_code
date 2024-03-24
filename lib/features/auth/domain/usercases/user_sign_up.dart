5e24d534cecc00ee16ee01b2958c1c18574c57bf
Darpan Neve <darpanneve3@gmail.com>
2024-03-24 12:12:00 +0530

    bloc added

--- lib/features/auth/domain/usercases/user_sign_up.dart
+++ lib/features/auth/domain/usercases/user_sign_up.dart
@@ -1,1 +1,26 @@
-class SignUp {}
+import 'package:flutter_clean_code/core/error/failure.dart';
+import 'package:flutter_clean_code/core/usecases/usecase.dart';
+import 'package:fpdart/fpdart.dart';
+
+import '../repository/auth_repository.dart';
+
+class SignUp implements UseCase<String, UserSignUpParams> {
+  final AuthRepository repository;
+
+  SignUp(this.repository);
+
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await repository.signUpWithEmailAndPassword(
+        name: params.name, email: params.email, password: params.password);
+  }
+}
+
+class UserSignUpParams {
+  final String email;
+  final String password;
+  final String name;
+
+  UserSignUpParams(
+      {required this.email, required this.password, required this.name});
+}
